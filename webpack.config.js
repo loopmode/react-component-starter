7ed4dbc99dcef0a48fa054612a34681592b760ee
Jovica <jovica.aleksic@loopmode.de>
2015-09-11 00:53:03 +0200

    added css/sass support and utils

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,60 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var autoprefixer = require('autoprefixer');
+
 
 var SRC_PATH = path.resolve(__dirname, 'src');
+
+
 var DEV_SERVER = {
     IP: '127.0.0.1',
     PORT: 8888
 };
+
+
+const LOADERS = {
+    ESLINT: 'eslint',
+    REACT_HOT: 'react-hot',
+    BABEL: 'babel',
+    FLOWCHECK: 'flowcheck',
+
+    STYLE: 'style',
+    CSS: 'css?localIdentName=[path][name]---[local]---[hash:base64:5]',
+    SASS: 'sass?sourceMap&includePaths[]=' + SRC_PATH,
+    POST_CSS: 'postcss',
+};
+
+
 module.exports = {
     debug: true,
+
     ip: DEV_SERVER.IP,
     port: DEV_SERVER.PORT,
-    entry: [
-        path.join(SRC_PATH, 'example.js'),
-        'webpack-dev-server/client?http://' + DEV_SERVER.IP + ':' + DEV_SERVER.PORT,
-        'webpack/hot/only-dev-server',
-    ],
-    module: {
-        preLoaders: [{
-            include: SRC_PATH,
-            test: /\.jsx?$/,
-            loaders: ['eslint'],
-        }, ],
-        loaders: [{
-            include: SRC_PATH,
-            test: /\.jsx?$/,
-            loaders: [
-                'react-hot',
-                'babel',
-                'flowcheck',
-                'babel?blacklist=flow'
-            ],
-        }, ],
-    },
+
     resolve: {
         extensions: ['', '.js', '.jsx'],
         root: SRC_PATH
     },
+
+    entry: [
+        path.join(SRC_PATH, 'preview.js'),
+        'webpack-dev-server/client?http://' + DEV_SERVER.IP + ':' + DEV_SERVER.PORT,
+        'webpack/hot/only-dev-server',
+    ],
+
     output: {
         path: __dirname,
         filename: 'bundle.js',
         publicPath: '/',
     },
+
+    postcss: [
+        autoprefixer({
+            browsers: ['last 2 versions']
+        })
+    ],
+
     plugins: [
         new webpack.DefinePlugin({
             'process.env': {
@@ -54,4 +67,39 @@ module.exports = {
             title: path.basename(__dirname)
         }),
     ],
+
+    module: {
+
+        preLoaders: [{
+            test: /\.jsx?$/,
+            include: SRC_PATH,
+            loaders: [LOADERS.ESLINT],
+        }],
+
+        loaders: [{
+            test: /\.jsx?$/,
+            include: SRC_PATH,
+            loaders: [
+                LOADERS.REACT_HOT,
+                LOADERS.FLOWCHECK,
+                LOADERS.BABEL + '?blacklist=flow'
+            ],
+        }, {
+            test: /\.css$/,
+            loaders: [
+                LOADERS.STYLE,
+                LOADERS.CSS,
+                LOADERS.POST_CSS
+            ],
+        }, {
+            test: /\.scss$/,
+            loaders: [
+                LOADERS.STYLE,
+                LOADERS.CSS,
+                LOADERS.SASS,
+                LOADERS.POST_CSS
+            ],
+        }]
+    },
+
 };
